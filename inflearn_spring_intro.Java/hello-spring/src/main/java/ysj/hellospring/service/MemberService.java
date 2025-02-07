10e90417c27eeb1985dcf6e8c4b52d16bc10d072
Yoon <ysj0509@sookmyung.ac.kr>
2025-02-07 20:19:07 +0900

    Course 6-1. AOP 사용 전 문제상황
    - 모든 메소드의 호출 시간 측정하기
      - 모든 메소드에 try-catch 문 및 시간 측정 변수 추가
      => 시간 측정 로직과 핵심 비즈니스 로직이 섞임
      => 공통 로직(공통 관심 사항)임에도 별도로 분리하기 어려움
      => 중복 코드 -> 로직 변경 시 모든 로직을 찾아가며 변경해야 함

--- inflearn_spring_intro.Java/hello-spring/src/main/java/ysj/hellospring/service/MemberService.java
+++ inflearn_spring_intro.Java/hello-spring/src/main/java/ysj/hellospring/service/MemberService.java
@@ -20,11 +20,16 @@ public class MemberService {
     
     // 회원 가입
     public Long join(Member member) {
-        // 중복 회원 금지
-        validateDuplicatedMember(member);
-
-        memberRepository.save(member);
-        return member.getId();
+        long start = System.currentTimeMillis();
+        try {
+            validateDuplicatedMember(member); //중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join " + timeMs + "ms");
+        }
     }
 
     private void validateDuplicatedMember(Member member) {
@@ -35,7 +40,14 @@ public class MemberService {
     }
 
     public List<Member> findMembers() {
-        return memberRepository.findAll();
+        long start = System.currentTimeMillis();
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers " + timeMs + "ms");
+        }
     }
     public Optional<Member> findOne(Long memberId) {
         return memberRepository.findById(memberId);
