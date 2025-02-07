785a916160ced091c1068c9345f1faa9ce36078d
Yoon <ysj0509@sookmyung.ac.kr>
2025-02-07 19:35:59 +0900

    Course 5-4. JDBC Template 로 서버-DB 매핑

--- inflearn_spring_intro.Java/hello-spring/src/main/java/ysj/hellospring/SpringConfig.java
+++ inflearn_spring_intro.Java/hello-spring/src/main/java/ysj/hellospring/SpringConfig.java
@@ -3,6 +3,7 @@ package ysj.hellospring;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ysj.hellospring.repository.JdbcMemberRepository;
+import ysj.hellospring.repository.JdbcTemplateMemberRepository;
 import ysj.hellospring.repository.MemberRepository;
 import ysj.hellospring.repository.MemoryMemberRepository;
 import ysj.hellospring.service.MemberService;
@@ -25,6 +26,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
